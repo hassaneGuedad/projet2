f53623c4ec30cb3f88f3081737456f61b0c34780
HASSANE GUEDAD <115504253+hassaneGuedad@users.noreply.github.com>
2025-07-17 14:13:57 +0100

    Add src/components/Navbar/Navbar.js

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="/" className="navbar-logo">MonProjetReact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
