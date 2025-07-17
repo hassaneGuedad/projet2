15321ada8a3500c712ff434d98070f0714ea857c
HASSANE GUEDAD <115504253+hassaneGuedad@users.noreply.github.com>
2025-07-17 14:13:58 +0100

    Add src/pages/HomePage/HomePage.js

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Bienvenue sur MonProjetReact</h1>
+      <p>Ceci est la page d'accueil</p>
+    </div>
+  );
+}
+
+export default HomePage;
